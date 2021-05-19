61ba3dbf8720af8911c001827b23b0cf7cea0967
JeevithaSubramanian-prog <73568761+JeevithaSubramanian-prog@users.noreply.github.com>
2021-05-19 18:59:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor(){
+    document.getElementById("heading").style.color = "green";
+}
+function flip()
+{
+    document.getElementById("img1").src="https://th.bing.com/th/id/R152d188756261446a19c67785ddedb84?rik=0cZDKtqwB62Xhw&riu=http%3a%2f%2fwallpapercave.com%2fwp%2f9061Ujo.jpg&ehk=CdrD2I8L%2f7TFuOxqgrFD5xBHTVFSMu%2fUXB%2bJxoRUQcs%3d&risl=&pid=ImgRaw";
+}
